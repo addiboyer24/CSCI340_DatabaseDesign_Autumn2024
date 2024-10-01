0eaff2f8e622cab90a37f0709127eb832d39d804
Addison Boyer <ab240243@umconnect.umt.edu>
2024-09-30 21:33:57 -0600

    Adding files from class

--- 09_24_2024/src/Vehicle.java
+++ 09_24_2024/src/Vehicle.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Vehicle {
     public Vehicle(){
     }
@@ -5,6 +7,14 @@ public class Vehicle {
     public static void main(String[] args) {
         Car myCar = new Car();
         Truck myTruck = new Truck();
+
+        ArrayList<Vehicle> myGarage = new ArrayList<Vehicle>();
+        myGarage.add(myCar);
+        myGarage.add(myTruck);
+
+        for(int i = 0; i < myGarage.size(); i++){
+            myGarage.get(i).honk();
+        }
     }
     public void honk(){
         System.out.println("Beep!!");
